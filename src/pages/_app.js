220e5a7812e020da93bbc17da9fd405413728c6e
Boonsuen Oh <boonsuenoh@gmail.com>
2020-12-30 17:33:47 +0800

    Rename icon.svg to favicon.svg

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,7 @@
 import Head from 'next/head';
 import GlobalStyle, { fontFaceRules } from '../components/GlobalStyle.css';
 
-import img_favicon from '../assets/img/icon.svg';
+import img_favicon from '../assets/img/favicon.svg';
 
 export default function App({ Component, pageProps }) {
   return (
